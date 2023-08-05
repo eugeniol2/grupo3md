8b5fc0fff3dd9c98f5bea84d2847446635106b19
Eugenio araújo <eugeniodornelesl2@gmail.com>
2023-08-05 10:46:09 -0300

    First commit

--- Home.py
+++ Home.py
@@ -1,10 +1,14 @@
 import streamlit as st
 import pandas as pd
 
-censo2020 = pd.read_csv("data\censo2020.CSV")
-censo2021 = pd.read_csv("data\censo2021.CSV")
-censo2020_2021_merged = pd.read_csv("data\merged_censo.CSV")
+censo2020 = pd.read_csv("data\censo2020_filtrado.CSV")
+censo2021 = pd.read_csv("data\censo2021_filtrado.CSV")
 
-st.write("censo2020")
-st.write(censo2020_2021_merged.head())
-st.write("Merged Censo Data")
+
+st.title("Censo 2020")
+st.write(censo2020.describe())
+st.title("Censo 2021")
+st.write(censo2021.describe())
+
+
+st.title("Streamlit")
