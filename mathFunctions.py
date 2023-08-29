8ea9ae3fc8d32c35b6985e626014d18f2006d6b2
Claudiano Lima <claudianopereira047@gmail.com>
2023-08-29 20:50:34 -0300

    feat: adding math functions

---
+++ mathFunctions.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+def calcular_taxa_evasao(row):
+    M_n = row["QT_MAT_2021"]
+    In_n = row["QT_ING_2021"]
+    M_n_1 = row["QT_MAT_2020"]
+    Eg_n_1 = row["QT_CONC_2020"]
+
+    if M_n_1 - Eg_n_1 == 0:
+        return None
+
+    return (1 - (M_n - In_n) / (M_n_1 - Eg_n_1)) * 100
+
+
+def userCalculator():
+    st.title("Calculadora de taxa de evasão")
+    # User inputs
+    M_n = st.number_input("Quantidade matriculados 2021, QT_MAT_2021", min_value=0)
+    In_n = st.number_input("Quantidade ingressantes 2021 QT_ING_2021", min_value=0)
+    M_n_1 = st.number_input("Quantidade matriculados 2020, QT_MAT_2020", min_value=0)
+    Eg_n_1 = st.number_input("Quantidade concluintes 2020, QT_CONC_2020", min_value=0)
+
+    if M_n_1 - Eg_n_1 == 0:
+        st.warning("Division by zero. Cannot calculate abandonment rate.")
+    else:
+        abandonment_rate = (1 - (M_n - In_n) / (M_n_1 - Eg_n_1)) * 100
+        st.subheader(f"Resultado: {abandonment_rate:.2f}%")
